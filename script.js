3fb28b5dd4eac31e5851c8646a0d870dcf5e0ebe
Ananya Kaushal <62956565+Ananya-Kaushal@users.noreply.github.com>
2024-07-19 20:14:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 function fibonacci(num) {
 // your code here
-	let a=0;
-	let b=1;
-	if(num==1)return a;
-	else if(num==2)return b;
-	else
-	{
-		let p=fibonacci(num-2)+fibonacci(num-3);
-		return p;
-	}
+	// let a=0;
+	// let b=1;
+	// if(num==1)return a;
+	// else if(num==2)return b;
+	// else
+	// {
+	// 	let p=fibonacci(num-2)+fibonacci(num-3);
+	// 	return p;
+	// }
+	let a = 0, b = 1, c, i;
+    if( num == 0)
+        return a;
+    for(i = 2; i <= num; i++)
+    {
+    c = a + b;
+    a = b;
+    b = c;
+    }
+    return b;
 	
 }
 
